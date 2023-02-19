2ac5943403772a1fd6e0c41c2010b2aa725cdd9a
otienoonyango2000 <otienoonyango2000@gmail.com>
2023-02-19 10:33:47 +0300

    first commit flow loop

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  i=10
+  while i > 0
+    puts i
+    i-=1
+  end 
+ puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,16 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
-def reverse_string(str)
+def reverse_string(string)
   # your code here
+  reverse_string = ""
+  string.length.times do |i|
+    reverse_string = string[i] + reverse_string
+  end
+  reverse_string
 end
